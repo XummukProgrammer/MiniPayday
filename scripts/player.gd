f254df579c1bbf2f36ab2b39c8be07dd15d78495
Andrey Grebenshchikov <a-grebenshikov@bk.ru>
2023-03-28 01:30:12 +0500

    Добавлено передвижение персонажа

---
+++ scripts/player.gd
@@ -0,0 +1,22 @@
+extends KinematicBody2D
+
+const SPEED = 200
+
+var velocity = Vector2()
+
+func _physics_process(delta):
+	if Input.is_action_pressed("ui_left"):
+		velocity.x = -SPEED
+	elif Input.is_action_pressed("ui_right"):
+		velocity.x = SPEED
+	else:
+		velocity.x = 0
+	
+	if Input.is_action_pressed("ui_up"):
+		velocity.y = -SPEED
+	elif Input.is_action_pressed("ui_down"):
+		velocity.y = SPEED
+	else:
+		velocity.y = 0
+
+	move_and_slide(velocity, Vector2(0, -1))
