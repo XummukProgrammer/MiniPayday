2445b39e2185ee11fe76b92261b167dc4be1611d
Andrey Grebenshchikov <a-grebenshikov@bk.ru>
2023-04-09 02:02:02 +0500

    Доработал перемещение игрока

--- scripts/characters/enemy.gd
+++ scripts/characters/enemy.gd
@@ -1,27 +1,27 @@
 class_name enemy extends character
 
-var _start_position: Vector2
-onready var _path = $Path2D
+export var _path_to_move_pattern: NodePath
 
 var _current_point = 0
-var _time = 0
+
+func get_move_pattern() -> Path2D:
+	return get_node(_path_to_move_pattern) as Path2D
 
 func _physics_process(delta):
-	_time = _time + delta
-	
-	if _time < 0.001:
-		return
+	var move_pattern = get_move_pattern()
+	if move_pattern:
+		var point = move_pattern.curve.get_point_position(_current_point)
+		var dir = (point - position).normalized()
+		position = position + dir * delta * get_speed()
 		
-	_time = 0
-	
-	var points = _path.curve.get_baked_points()
-	var point = points[_current_point]
-	position = _start_position + point
-	var cur_pos = position
-	_current_point = _current_point + 1
-	
-	if _current_point >= points.size():
-		_current_point = 0
+		if _is_vectors_equal(position, point):
+			_current_point = _current_point + 1
+			
+			if _current_point >= move_pattern.curve.get_point_count():
+				_current_point = 0
 
-func _ready():
-	_start_position = position
+func _is_floats_equal(var a: float, var b: float):
+	return abs(a - b) < 1
+
+func _is_vectors_equal(var a: Vector2, var b: Vector2):
+	return _is_floats_equal(a.x, b.x) and _is_floats_equal(a.y, b.y)
