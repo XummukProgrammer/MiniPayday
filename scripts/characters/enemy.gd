19fd4c6a6724b8461bfa51b10ae72442672da71e
Andrey Grebenshchikov <a-grebenshikov@bk.ru>
2023-04-09 17:29:57 +0500

    Доделал поворот в нужную сторону при движении врага

--- scripts/characters/enemy.gd
+++ scripts/characters/enemy.gd
@@ -3,6 +3,7 @@ class_name enemy extends character
 export var _path_to_move_pattern: NodePath
 
 var _current_point = 0
+var _start_rotation = 0
 
 func get_move_pattern() -> Path2D:
 	return get_node(_path_to_move_pattern) as Path2D
@@ -13,13 +14,28 @@ func _physics_process(delta):
 		var point = move_pattern.curve.get_point_position(_current_point)
 		var dir = (point - position).normalized()
 		position = position + dir * delta * get_speed()
-		rotation = dir.angle()
 		
 		if _is_vectors_equal(position, point):
 			_current_point = _current_point + 1
 			
+			# Для правильной работы необходимо сомкнуть прямую.
 			if _current_point >= move_pattern.curve.get_point_count():
 				_current_point = 0
+				
+			_change_move_pattern_point(_current_point)
+
+func _ready():
+	_start_rotation = rotation
+	_change_move_pattern_point(_current_point)
+
+func _draw():
+	#draw_polygon(
+	#	$eye/CollisionPolygon2D.polygon, PoolColorArray([Color(1, 1, 1, 1)])
+	#)
+	pass
+
+func _on_eye_body_exited(body):
+	pass # Replace with function body.
 
 func _is_floats_equal(var a: float, var b: float):
 	return abs(a - b) < 1
@@ -27,10 +43,10 @@ func _is_floats_equal(var a: float, var b: float):
 func _is_vectors_equal(var a: Vector2, var b: Vector2):
 	return _is_floats_equal(a.x, b.x) and _is_floats_equal(a.y, b.y)
 
-func _draw():
-	draw_polygon(
-		$eye/CollisionPolygon2D.polygon, PoolColorArray([Color(1, 1, 1, 1)])
-	)
-
-func _on_eye_body_exited(body):
-	pass # Replace with function body.
+func _change_move_pattern_point(var point_index: int):
+	var move_pattern = get_move_pattern()
+	if move_pattern:
+		var point = move_pattern.curve.get_point_position(point_index)
+		if point:
+			var dir = (point - position).normalized()
+			rotation = _start_rotation + dir.angle()
